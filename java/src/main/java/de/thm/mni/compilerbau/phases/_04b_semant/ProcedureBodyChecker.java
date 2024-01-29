d0cfc912fd92cddc33746423ab268c344e230278
kkls71 <kevin.klauser@mni.thm.de>
2024-01-29 11:19:50 +0100

    finished 4b code, some more errors are needed tho

--- java/src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
+++ java/src/main/java/de/thm/mni/compilerbau/phases/_04b_semant/ProcedureBodyChecker.java
@@ -4,16 +4,14 @@ import de.thm.mni.compilerbau.CommandLineOptions;
 import de.thm.mni.compilerbau.absyn.*;
 import de.thm.mni.compilerbau.absyn.visitor.DoNothingVisitor;
 import de.thm.mni.compilerbau.absyn.visitor.Visitor;
-import de.thm.mni.compilerbau.phases._04a_tablebuild.TableBuilder;
 import de.thm.mni.compilerbau.table.*;
 import de.thm.mni.compilerbau.table.SymbolTable;
 import de.thm.mni.compilerbau.types.ArrayType;
+import de.thm.mni.compilerbau.types.PrimitiveType;
 import de.thm.mni.compilerbau.types.Type;
-import de.thm.mni.compilerbau.utils.NotImplemented;
 
 import java.util.List;
 import java.util.Objects;
-import java.util.function.BinaryOperator;
 
 /**
  * This class is used to check if the currently compiled SPL program is semantically valid.
@@ -42,16 +40,17 @@ public class ProcedureBodyChecker {
         NamedVariable currentArraySimpleVar;
         NamedVariable currentNamedVar;
         int currentArrayType;
+        int previousArrayType;
         int leftTypeOfAssign;
         int leftArraySize;
         boolean assignStmFlag;
         boolean assignLeftArray;
         boolean rightSideOfAssign;
-        boolean currentOpExpIntType;
+        boolean isCurrentOpExpIntType;
+        Type currentOpExpIntType;
         boolean leftArrayWithoutIndex;
-        boolean isCurrentOperatorInt;
-        boolean isCurrentOpLeftInt;
-        boolean isCurrentOpRightInt;
+        Type currentLeftOpExpIntType;
+        Type currentRightOpExpIntType;
 
         TypeAnalysisVisitor(SymbolTable table){
             this.globalTable = table;
@@ -85,7 +84,8 @@ public class ProcedureBodyChecker {
 
         @Override
         public void visit(IntLiteral intLiteral) {
-            this.isCurrentOperatorInt = true;
+            this.currentOpExpIntType = PrimitiveType.intType;
+            this.isCurrentOpExpIntType = true;
         }
 
         @Override
@@ -95,23 +95,30 @@ public class ProcedureBodyChecker {
 
         @Override
         public void visit(BinaryExpression binaryExpression) {
-            /*binaryExpression.leftOperand.accept(this);
-            isCurrentOpLeftInt = isCurrentOperatorInt;
+            binaryExpression.leftOperand.accept(this);
+            currentLeftOpExpIntType = currentOpExpIntType;
             binaryExpression.rightOperand.accept(this);
-            isCurrentOpRightInt = isCurrentOperatorInt;
-            if(!isCurrentOpRightInt) {
+            currentRightOpExpIntType = currentOpExpIntType;
+
+            if(currentRightOpExpIntType != currentLeftOpExpIntType) {
                 binaryTypeMismatch(binaryExpression.operator, binaryExpression.leftOperand, binaryExpression.rightOperand);
-            }*/
-
-        }
-
-        @Override
-        public void visit(NamedTypeExpression namedTypeExpression) {
-
+            }
+            currentOpExpIntType = currentLeftOpExpIntType;
+            if(currentLeftOpExpIntType.byteSize != 4 || currentRightOpExpIntType.byteSize != 4){
+                binaryTypeMismatch(binaryExpression.operator, currentLeftOpExpIntType, currentRightOpExpIntType);
+            }
         }
 
         @Override
         public void visit(UnaryExpression unaryExpression) {
+            unaryExpression.operand.accept(this);
+            if(currentOpExpIntType.byteSize != 4) {
+                unaryTypeMismatch(unaryExpression.operator, currentOpExpIntType);
+            }
+        }
+
+        @Override
+        public void visit(NamedTypeExpression namedTypeExpression) {
 
         }
 
@@ -136,7 +143,8 @@ public class ProcedureBodyChecker {
                 this.currentArraySimpleVar = namedVariable;
             }
 
-            currentOpExpIntType = ((VariableEntry) entry).type.byteSize == 4;
+            isCurrentOpExpIntType = ((VariableEntry) entry).type.byteSize == 4;
+            currentOpExpIntType = ((VariableEntry) entry).type;
         }
 
         @Override
@@ -148,7 +156,38 @@ public class ProcedureBodyChecker {
 
         @Override
         public void visit(VariableExpression variableExpression) {
+            //TODO: Schauen ob das richtig ist, idk
+            if(variableExpression.variable instanceof ArrayAccess) {
+                previousArrayType = currentArrayType;
+                currentArrayType = 0;
+            }
 
+            variableExpression.variable.accept(this);
+            VariableEntry entry = (VariableEntry) (localTable.lookup(currentArraySimpleVar.name));
+
+            if(variableExpression.variable instanceof ArrayAccess) {
+                int size = entry.type.byteSize;
+                if(size != currentArrayType) {
+                    invalidArrayAccess(entry.type);
+                }
+                isCurrentOpExpIntType = true;
+                currentOpExpIntType =  PrimitiveType.intType;
+            }
+
+            if(assignStmFlag){
+                if(variableExpression.variable instanceof ArrayAccess && !assignLeftArray){
+                    int currentType = entry.type.byteSize;
+
+                    if(leftTypeOfAssign != (currentArrayType - currentType)){
+                        assignmentTypeMismatch(entry.type, PrimitiveType.intType);
+                    }
+                }else if(leftTypeOfAssign != 0 && !assignLeftArray){
+                    assignmentTypeMismatch(entry.type, new ArrayType(PrimitiveType.intType, leftArraySize));
+                }
+                if(leftArrayWithoutIndex && rightSideOfAssign){
+                    assignmentTypeMismatch(entry.type, PrimitiveType.intType);
+                }
+            }
         }
 
         @Override
@@ -201,7 +240,9 @@ public class ProcedureBodyChecker {
                     invalidArrayAccess(entry.type);
                 }
 
-                currentOpExpIntType = true;
+                isCurrentOpExpIntType = true;
+                currentOpExpIntType = PrimitiveType.intType;
+
                 leftTypeOfAssign = currentArrayType - 1;
             }else{
                 if(assignLeftArray){
@@ -226,10 +267,43 @@ public class ProcedureBodyChecker {
 
         }
 
+        @Override
+        public void visit(IfStatement ifStatement) {
+            if(!(ifStatement.condition instanceof BinaryExpression)) {
+                ifConditionTypeMismatch(ifStatement.condition);
+            }
+
+            ifStatement.condition.accept(this);
+            ifStatement.thenPart.accept(this);
+            ifStatement.elsePart.accept(this);
+        }
+
+        @Override
+        public void visit(WhileStatement whileStatement) {
+            if(!(whileStatement.condition instanceof BinaryExpression)) {
+                whileConditionTypeMismatch(whileStatement.condition);
+            }
+
+            whileStatement.condition.accept(this);
+            whileStatement.body.accept(this);
+        }
+
         //-----Errors------
         void undefinedIdentifier(Identifier identifier) {
             System.err.println("Identifier '" + identifier + "' is not defined.");
             System.exit(101);
+        }
+        void assignmentTypeMismatch(Type type1, Type type2) {
+            System.err.println("A value of type '" + type1 + "' can not be assigned to variable of type '" + type2 + "'.");
+            System.exit(108);
+        }
+        void ifConditionTypeMismatch(Expression type) {
+            System.err.println("'if' condition expected to be of type 'boolean', but is of type '" + type + "'.");
+            System.exit(110);
+        }
+        void whileConditionTypeMismatch(Expression type) {
+            System.err.println("while' condition expected to be of type 'boolean', but is of type '" + type + "'.");
+            System.exit(111);
         }
         void identifierNotAProcedure(Identifier identifier) {
             System.err.println("Identifier '" + identifier + "' does not refer to a procedure.");
@@ -242,6 +316,14 @@ public class ProcedureBodyChecker {
         void binaryTypeMismatch(BinaryExpression.Operator binaryOperator, Expression type1, Expression type2) {
             System.err.println("Type mismatch in binary expression: Operator '" + binaryOperator + "' does not accept operands of types '" + type1 + "' and '" + type2 + "'.");
             System.exit(118);
+        }
+        void binaryTypeMismatch(BinaryExpression.Operator binaryOperator, Type type1, Type type2) {
+            System.err.println("Type mismatch in binary expression: Operator '" + binaryOperator + "' does not accept operands of types '" + type1 + "' and '" + type2 + "'.");
+            System.exit(118);
+        }
+        void unaryTypeMismatch(UnaryExpression.Operator operator, Type type) {
+            System.err.println("Type mismatch in unary expression: Operator '" + operator + "' does not accept operand of type '" + type + "'.");
+            System.exit(119);
         }
         void invalidArrayAccess(Type type) {
             System.err.println("Type mismatch: Invalid array access operation on non-array variable of type '" + type + "'.");
